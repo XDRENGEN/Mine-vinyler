b16b50e34edf928c8e5664a98f120957ac5af9db
xdrengen <xdrengen@gmail.com>
2024-11-06 17:08:41 +0100

    Tilføj funktion tilføjet og virker. Bliver gemt lokalt i en JSON.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
 const app = Vue.createApp({
     data() {
         return {
-            intro: 'Welcome to my vue template',
-        }
+            intro: 'Velkommen til min vinylsamling.',
+            liste: JSON.parse(localStorage.getItem('vinylliste')) || [],
+
+            newVinyl: {
+                id: null,
+                artist: '',
+                album: '',
+                year: '',
+                genre: '',
+                label: '',
+            },
+        };
     },
 
 
@@ -14,9 +24,34 @@ const app = Vue.createApp({
 
 
     methods: {
-        myMethod(){
-        
-        }
+        addMethod(){
+            if (
+                this.newVinyl.id &&
+                this.newVinyl.artist &&
+                this.newVinyl.album &&
+                this.newVinyl.year &&
+                this.newVinyl.genre &&
+                this.newVinyl.label
+            ) {
+                this.liste.push({ ...this.newVinyl });
+
+                localStorage.setItem('vinylliste', JSON.stringify(this.liste));
+
+ 
+
+                this.newVinyl = {
+                    id: null,
+                    artist: '',
+                    album: '',
+                    year: '',
+                    genre: '',
+                    label: '',
+                };
+
+            } else {
+                alert('Udfyld venligst alle felter');
+            }
+        },
     },
 
 
